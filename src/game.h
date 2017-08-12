609ad6979b85af3fa4ea08d50f65c098ad0b676d
Wang Xu <wangxu298@foxmail.com>
2017-08-12 23:52:29 +0800

    added some basic test operators

---
+++ src/game.h
@@ -0,0 +1,50 @@
+//
+// Created by wang on 8/11/17.
+//
+
+#ifndef INC_24GAME_GAME_H
+#define INC_24GAME_GAME_H
+
+
+template <typename Arg, typename Result>
+struct unary_function {
+    typedef Arg argument_type;
+    typedef Result result_type;
+};
+
+template <typename Arg1, typename Arg2, typename Result>
+struct binary_function {
+    typedef Arg1 first_argument_type;
+    typedef Arg2 second_argument_type;
+    typedef Result result_type;
+};
+
+template <typename T>
+struct Or : public unary_function<T, T> {
+    T operator()(const T& x) const {
+        return ~x;  // TODO class T requires override ~ operator
+    }
+};
+
+template <typename T>
+struct Plus : public binary_function<T, T, T> {
+    T operator()(const T& x, const T& y) const {
+        return x + y;
+    }
+};
+
+template <typename T>
+struct Minus : public binary_function<T, T, T> {
+    T operator()(const T& x, const T& y) const {
+        return x - y;
+    }
+};
+
+//  test operators above
+
+
+
+
+
+
+#endif //INC_24GAME_GAME_H
